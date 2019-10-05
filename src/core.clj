9b77084fc72af267fe7df448dff85a800c84e6fe
Hans Bugge Grathwohl <hans.bugge@deondigital.com>
2019-10-05 10:31:16 +0200

    more work

--- src/core.clj
+++ src/core.clj
@@ -1,21 +1,33 @@
 (ns clotract.core)
 
-(defn withdraw [{:keys [balance] :as state} amount]
+(defn withdraw
+  [{:keys [balance] :as state}
+   {:keys [amount] :as command}]
   (assert (> amount 0) "amount must be positive")
-  (if (> amount balance)
-    (throw (ex-info "Amount too high" state))
-    (assoc state :balance (- balance amount))))
+  (assert (> balance amount) "Amount too high")
+  (assoc state :balance (- balance amount)))
 
-(defn deposit [{:keys [balance] :as state} amount]
+(defn deposit
+  [{:keys [balance] :as state}
+   {:keys [amount]}]
   (assert (> amount 0) "amount must be positive")
   (assoc state :balance (+ balance amount)))
 
 (comment
-  (withdraw {:balance 100 :name "Johnny"} 50)
-  (withdraw {:balance 10} 20)
-  (withdraw {:balance 10} -1)
-  (deposit {:balance 100} 50)
-  (deposit {:balance 100} -50)
+  (withdraw {:balance 100 :name "Johnny"} {:amount 50})
+  (withdraw {:balance 10} {:amount 20})
+  (withdraw {:balance 10} {:amount -1})
+  (deposit {:balance 100} {:amount 50})
+  (deposit {:balance 100} {:amount -50})
+  )
 
+(comment
+  (def johnny-account (atom {:name "Johnny B"
+                             :balance 1000}))
 
+  (swap! johnny-account #(withdraw % {:amount 100}))
+
+  @johnny-account
+
+  (swap! johnny-account #(withdraw % {:amount -900}))
   )

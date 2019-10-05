f9c6125cbcf6da40a1271a4d1ef7ca7bce4d7cf9
Hans Bugge Grathwohl <hans.bugge@deondigital.com>
2019-10-05 11:03:50 +0200

    checkpoint

--- src/core.clj
+++ src/core.clj
@@ -2,10 +2,11 @@
 
 (defn withdraw
   [{:keys [balance] :as state}
-   {:keys [amount] :as command}]
+   {:keys [amount]}]
   (assert (> amount 0) "amount must be positive")
   (assert (> balance amount) "Amount too high")
   (assoc state :balance (- balance amount)))
+
 
 (defn deposit
   [{:keys [balance] :as state}
@@ -21,11 +22,55 @@
   (deposit {:balance 100} {:amount -50})
   )
 
+
+(defn withdraw-curry
+  [{:keys [amount]}]
+  (assert (> amount 0) "amount must be positive")
+  (fn [{:keys [balance] :as state}]
+    (assert (>= balance amount) "Amount too high")
+    (assoc state :balance (- balance amount))))
+
+(defn deposit-curry
+  [{:keys [amount]}]
+  (assert (> amount 0) "amount must be positive")
+  (fn [state]
+    (update state :balance (partial + amount))))
+
+(comment
+  ((withdraw-curry {:amount 10}) {:balance 10})
+
+
+  )
+
+
+(comment
+  (def acc (atom {:state {:balance 0}
+                  :ledger []}))
+
+  (def command `(deposit-curry {:amount 100}))
+  (let [{:keys [state history]} @acc
+        new-state ((eval command) state)
+        new-history (conj history command)]
+    (reset! acc {:state new-state :history new-history}))
+  )
+
+;; (def step eval)
+
+;; (defn step [event-list command]
+;;   (try (eval command)
+;;        (catch Exception e (str ))))
+
+;; (comment
+;;   (step `(deposit {:balance 0} {:amount 100}))
+;;   )
+
 (comment
   (def johnny-account (atom {:name "Johnny B"
                              :balance 1000}))
 
-  (swap! johnny-account #(withdraw % {:amount 100}))
+  (def withdraw-a-hundred (withdraw-curry {:amount 100}))
+
+  (swap! johnny-account withdraw-a-hundred)
 
   @johnny-account
 

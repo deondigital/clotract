275c797440c436a6c5240054e5c5b17a8b2bf9e8
Hans Bugge Grathwohl <hans.bugge@deondigital.com>
2019-10-05 11:53:25 +0200

    verify!

--- src/core.clj
+++ src/core.clj
@@ -2,6 +2,11 @@
 
 ;;; Commands
 
+(defn create-account []
+  (fn [state]
+    (assert (= nil state) "Initial state must be nil")
+    {:balance 0}))
+
 (defn withdraw
   [{:keys [amount]}]
   (assert (> amount 0) "amount must be positive")
@@ -15,6 +20,8 @@
   (fn [state]
     (update state :balance (partial + amount))))
 
+;;; Ledger
+
 (defn step [{:keys [state ledger] :as contract-state} command]
   (let [new-state
         (try ((eval command) state)
@@ -23,13 +30,57 @@
                                (assoc contract-state
                                       :command-exception e)))))]
     {:state new-state
-     :ledger (conj ledger command)}))
+     :ledger (vec (conj ledger command))}))
+
+
+(defn verify! [{:keys [state ledger]}]
+  (let [derived-state (:state (reduce step {} ledger))]
+    (if-not
+        (= state
+           derived-state)
+      (throw (ex-info "Could not validate contract object"
+                      {:expected-state state
+                       :derived-state derived-state})))))
+
+(comment
+  (def good (reduce step {} `[(create-account)
+                              (deposit {:amount 60})
+                              (deposit {:amount 40})]))
+
+  (def bad (update-in good [:state :balance] #(- % 1000)))
+
+  (verify! good)
+  (verify! bad)
+
+
+
+  (verify {:state {:balance 100}
+           :ledger `[(create-account)
+                     (deposit {:amount 60})
+                     (deposit {:amount 40})]})
+
+  )
+
+(defn step [{:keys [state ledger] :as contract-state} command]
+  (let [new-state
+        (try ((eval command) state)
+             (catch Error e
+               (throw (ex-info "Unexpected event"
+                               (assoc contract-state
+                                      :command-exception e)))))]
+    {:state new-state
+     :ledger (into [] (conj ledger command))}))
 
 (comment
   (step (step {:state {:balance 0}
                :ledger []}
               `(deposit {:amount 100}))
         `(deposit {:amount 50}))
+
+  (reduce step
+          {:state {:balance 0} :ledger []}
+          `[(deposit {:amount 100})
+            (deposit {:amount 50})])
 
   (reduce step
           {:state {:balance 0} :ledger []}
@@ -49,4 +100,3 @@
         new-history (conj history command)]
     (reset! acc {:state new-state :history new-history}))
   )
-

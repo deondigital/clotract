4db0b7afec8cec49c940296ed07fd1b68d2f99f9
Hans Bugge Grathwohl <hans.bugge@deondigital.com>
2019-10-05 11:55:11 +0200

    clean up

--- src/core.clj
+++ src/core.clj
@@ -32,45 +32,6 @@
     {:state new-state
      :ledger (vec (conj ledger command))}))
 
-
-(defn verify! [{:keys [state ledger]}]
-  (let [derived-state (:state (reduce step {} ledger))]
-    (if-not
-        (= state
-           derived-state)
-      (throw (ex-info "Could not validate contract object"
-                      {:expected-state state
-                       :derived-state derived-state})))))
-
-(comment
-  (def good (reduce step {} `[(create-account)
-                              (deposit {:amount 60})
-                              (deposit {:amount 40})]))
-
-  (def bad (update-in good [:state :balance] #(- % 1000)))
-
-  (verify! good)
-  (verify! bad)
-
-
-
-  (verify {:state {:balance 100}
-           :ledger `[(create-account)
-                     (deposit {:amount 60})
-                     (deposit {:amount 40})]})
-
-  )
-
-(defn step [{:keys [state ledger] :as contract-state} command]
-  (let [new-state
-        (try ((eval command) state)
-             (catch Error e
-               (throw (ex-info "Unexpected event"
-                               (assoc contract-state
-                                      :command-exception e)))))]
-    {:state new-state
-     :ledger (into [] (conj ledger command))}))
-
 (comment
   (step (step {:state {:balance 0}
                :ledger []}
@@ -90,13 +51,25 @@
             (withdraw {:amount 1000})])
   )
 
-(comment
-  (def acc (atom {:state {:balance 0}
-                  :ledger []}))
 
-  (def command `(deposit-curry {:amount 100}))
-  (let [{:keys [state history]} @acc
-        new-state ((eval command) state)
-        new-history (conj history command)]
-    (reset! acc {:state new-state :history new-history}))
+(defn verify! [{:keys [state ledger]}]
+  (let [derived-state (:state (reduce step {} ledger))]
+    (if-not
+        (= state
+           derived-state)
+      (throw (ex-info "Could not validate contract object"
+                      {:expected-state state
+                       :derived-state derived-state})))))
+
+(comment
+  (def good
+    (reduce step {}
+            `[(create-account)
+              (deposit {:amount 60})
+              (deposit {:amount 40})]))
+
+  (def bad (update-in good [:state :balance] #(- % 1000)))
+
+  (verify! good)
+  (verify! bad)
   )
